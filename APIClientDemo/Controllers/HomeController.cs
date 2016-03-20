7f9f83d66689326a02a0400a1817f905191d0a23
motysaqa <moty_saqa@yahoo.com>
2016-03-20 09:33:34 +0200

    add the project

---
+++ APIClientDemo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APIClientDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ContentResult Index()
+        {
+            var client=new HttpClient();
+            var respone =  client.GetStringAsync("http://api.openweathermap.org/data/2.5/weather?id=281133&appid=b1b15e88fa797225412429c1c50c122a").Result;
+
+            return Content(respone);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ APIClientDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="APIClientDemo.MvcApplication" Language="C#" %>
